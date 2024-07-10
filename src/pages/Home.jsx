763fa582a9b24fac1166b6aff794de15d4da73c3
Sheriffdeen Makinde <sheriffdeenmakinde@gmail.com>
2024-07-10 15:51:27 +0100

    worked on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,16 +10,16 @@ const Home = () => {
     <div className="min-h-screen bg-gray-200">
       <Nav />
       <div className="flex flex-col items-center justify-center h-full overflow-hidden">
-        <h2 className="text-4xl font-bold text-red-600 mb-4 mt-28">
+        <h2 className="text-4xl font-bold text-red-600 mb-2 mt-24">
           Having an Emergency?
         </h2>
-        <p className="text-lg text-gray-700 mb-6">
+        <p className="text-lg text-gray-700 mb-12">
           Press the button below, help will arrive soon
         </p>
         <button className="pulse-button">
           <FontAwesomeIcon icon={faHandPointer} />
         </button>
-        <button className="mt-16 bg-white text-red-600 border border-red-600 px-8 py-4 text-lg rounded-lg hover:bg-red-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-400">
+        <button className="mt-16 bg-white text-red-600 border border-red-600 px-8 py-4 text-xl rounded-lg hover:bg-red-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-red-400 ">
           GO TO DASHBOARD
         </button>
       </div>
